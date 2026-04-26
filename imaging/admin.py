e20aafa64dfd6b55b568ff2dd441aad5969c0f15
Ashray Sikka <ashray15.sikka@gmail.com>
2026-04-26 18:04:38 -0400

    Creating a superuser

--- imaging/admin.py
+++ imaging/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Patient, Scan, Report
 
-# Register your models here.
+# registering models so they show up in the admin panel
+admin.site.register(Patient)
+admin.site.register(Scan)
+admin.site.register(Report)
